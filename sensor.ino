ab7b3a19a5b08f2047b6d48b87cac7b2607fb739
Farhan Labib Sahil <farhanlabibsahil@gmail.com>
2025-10-25 02:26:45 +0600

    Implement reading function for sensor data
    Add reading function to process sensor data

--- sensor.ino
+++ sensor.ino
@@ -1,1 +1,11 @@
-
+void reading() {
+  sensor = 0;
+  for (byte i = 0; i < 5; i++) {
+    s[i] = analogRead(4 - i);
+    (s[i] > th) ? s[i] = 0 : s[i] = 1;
+    sensor += s[i] * base[i];
+    // Serial.print(s[i]);
+    // Serial.print(" ");
+  }
+  // Serial.println(sensor, BIN);
+}
